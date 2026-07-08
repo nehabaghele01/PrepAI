f7b9fba1777b2aa13be7b4fad7f8ac33087e732c
nehabaghele01e <nehabaghele007@gmail.com>
2026-07-08 16:27:08 +0530

    ✨ Complete UI redesign with modern responsive dashboard

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import "./App.css";
+// import App from "./App.jsx";
+import "./App2.css";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
