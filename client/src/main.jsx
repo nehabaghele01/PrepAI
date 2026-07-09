8d5c4e82ce44b75e5b4160868a100c7800289554
nehabaghele01e <nehabaghele007@gmail.com>
2026-07-09 22:32:58 +0530

    UI fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "./Previous_App.css";
+import "./App.css";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
